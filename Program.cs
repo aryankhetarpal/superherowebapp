352c5467d6842a142647a49e4b7d72fb919d2d98
aryankhetarpal <138314019+aryankhetarpal@users.noreply.github.com>
2023-12-11 10:59:55 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace SuperHeroes.WebSite
+{
+    /// <summary>
+    /// The main entry point for the application
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The main method to start the application
+        /// </summary>
+        /// <param name="args"></param> Command-line arguments
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        /// <summary>
+        /// Configures and creates the host builder for the application.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,108 @@
+using SuperHeroes.WebSite.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace SuperHeroes.WebSite
+{
+    /// <summary>
+    /// Startup class for configuring  services and the HTTP request pipeline.
+    /// </summary>
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// Configure the services and adds them to the dependency injection container.
+        /// </summary>
+        /// <param name="services"></param> The service collection to configure.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //configure razor pages with runtime compilation for development.
+            services.AddRazorPages().AddRazorRuntimeCompilation();
+
+            //Add server side blazor support
+            services.AddServerSideBlazor();
+
+            //Add HTTP client for making HTTP requests
+            services.AddHttpClient();
+
+            //Add MVC controllers.
+            services.AddControllers();
+
+            //Add a transient service for the JSonFileProductService.
+            services.AddTransient<JsonFileProductService>();
+        }
+
+        /// <summary>
+        /// Configures the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app">The application builder.</param>
+        /// <param name="env">The hosting environment.</param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                //use developer exception page in development mode.
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            //Redirect HTTP requests to HTTPS
+            app.UseHttpsRedirection();
+
+            //serve static files
+            app.UseStaticFiles();
+
+            //Enable routing
+            app.UseRouting();
+
+            //Enable authorization
+            app.UseAuthorization();
+
+            //Configure endpoints for routing
+            app.UseEndpoints(endpoints =>
+            {
+                //Map Razor pages
+                endpoints.MapRazorPages();
+
+                //Map MVC Controllers
+                endpoints.MapControllers();
+
+                //Map Blazor hub
+                endpoints.MapBlazorHub();
+
+                // endpoints.MapGet("/products", (context) => 
+                // {
+                //     var products = app.ApplicationServices.GetService<JsonFileProductService>().GetProducts();
+                //     var json = JsonSerializer.Serialize<IEnumerable<Product>>(products);
+                //     return context.Response.WriteAsync(json);
+                // });
+            });
+
+            // Custom 404 error handling middleware
+            app.Use(async (context, next) =>
+            {
+                await next();
+
+                if (context.Response.StatusCode == 404 && !context.Response.HasStarted)
+                {
+                    // Handle 404 errors here
+                    context.Response.Redirect("/PageNotFound");
+                }
+            });
+        }
+    }
+}
